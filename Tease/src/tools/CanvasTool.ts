d8d29ce925d23751f05cade68b034c6753b715bb
Carlos López <carlos@blewblew.com>
2013-04-30 22:55:25 -0500

    merge CanvasTool.ts

--- Tease/src/tools/CanvasTool.ts
+++ Tease/src/tools/CanvasTool.ts
@@ -3,34 +3,16 @@
 class CanvasTool implements Tool {
     public displayName: string;
     public displayImagePath: string; // Image to be shown on the toolbar
-<<<<<<< HEAD
-    public properties: Object; // Supported properties
-    public defaultAttributes: AttributeList;
-    public defaultDOMElement: JQuery; // Vanilla DOM element to be inserted by Tool, WITHOUT any default attributes
-    public toolbarDOMElement: JQuery; // DOM Element for the tool button in the toolbar
-    public sizingToolAttributes: AttributeList;
-=======
     public properties: string[]; // Supported properties
     public defaultAttributes: {};
     public defaultDOMElement: JQuery; // Vanilla DOM element to be inserted by Tool, WITHOUT any default attributes
     public toolbarDOMElement: JQuery; // DOM Element for the tool button in the toolbar
     public sizingToolAttributes: {};
->>>>>>> BROKEN: Se remplazó AttributeList por {}
 
 
 
     constructor(public id: string) {
         this.displayName = 'Canvas';
-<<<<<<< HEAD
-        this.properties = new Object;
-        this.defaultAttributes = new AttributeList;
-
-        this.properties['width'] = new Property('width', 'ancho', 'width');
-        this.properties['height'] = new Property('height', 'largo', 'height');
-
-        this.defaultAttributes.setAttribute(new Attribute(this.properties['width'], '650'));
-        this.defaultAttributes.setAttribute(new Attribute(this.properties['height'], '500'));
-=======
         this.properties = [];
         this.defaultAttributes = {};
 
@@ -39,24 +21,15 @@ class CanvasTool implements Tool {
 
         this.defaultAttributes['width'] = '650';
         this.defaultAttributes['height'] = '500';
->>>>>>> BROKEN: Se remplazó AttributeList por {}
 
         this.defaultDOMElement = $('<div></div>');
     }
 
-<<<<<<< HEAD
-    public setAttributesInDOMElement(attributes: AttributeList, DOMElement: JQuery) {
-        
-        for (var i in attributes.attributes) {
-            var value = attributes.attributes[i].value;
-            switch (attributes.attributes[i].property.id) {
-=======
     public setAttributesInDOMElement(attributes: {}, DOMElement: JQuery) {
         
         for (var i in attributes) {
             var value = attributes[i];
             switch (i) {
->>>>>>> BROKEN: Se remplazó AttributeList por {}
                 case 'width':
                     DOMElement.css('width', value + 'px');
                     break;
@@ -70,11 +43,7 @@ class CanvasTool implements Tool {
     }
 
     public getAttributesFromDOMElement(DOMElement: JQuery) {
-<<<<<<< HEAD
-        var attributes = new Attribute[];
-=======
         var attributes = {};
->>>>>>> BROKEN: Se remplazó AttributeList por {}
         return attributes;
     }
 }
